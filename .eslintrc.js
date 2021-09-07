e51aee1d613056709e1cfb80101d045a98300eea
1devNdogs <vsalgado.a@gmail.com>
2021-09-07 10:07:07 -0300

    Fix format and lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
-const base = require('@polkadot/dev/config/eslint.cjs');
+const base = require('@open-web3/dev-config/config/eslint.cjs');
 
 module.exports = {
   ...base,
   ignorePatterns: [
+    '.eslintrc.cjs',
     '.eslintrc.js',
     '.github/**',
     '.vscode/**',
@@ -13,15 +14,10 @@ module.exports = {
   ],
   parserOptions: {
     ...base.parserOptions,
-    project: [
-      './tsconfig.json'
-    ]
+    project: ['./tsconfig.json']
   },
   rules: {
     ...base.rules,
-    // add override for any (a metric ton of them, initial conversion)
-    '@typescript-eslint/no-explicit-any': 'off',
-    // this seems very broken atm, false positives
-    '@typescript-eslint/unbound-method': 'off'
+    '@typescript-eslint/no-explicit-any': 'off'
   }
 };
