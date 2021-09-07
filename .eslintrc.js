0599a737528f6cc437dbc46e8e3d287ed34efb77
1devNdogs <vsalgado.a@gmail.com>
2021-09-07 08:36:37 -0300

    Add formatting and lint tools

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+const base = require('@polkadot/dev/config/eslint.cjs');
+
+module.exports = {
+  ...base,
+  ignorePatterns: [
+    '.eslintrc.js',
+    '.github/**',
+    '.vscode/**',
+    '.yarn/**',
+    '**/build/*',
+    '**/coverage/*',
+    '**/node_modules/*'
+  ],
+  parserOptions: {
+    ...base.parserOptions,
+    project: [
+      './tsconfig.json'
+    ]
+  },
+  rules: {
+    ...base.rules,
+    // add override for any (a metric ton of them, initial conversion)
+    '@typescript-eslint/no-explicit-any': 'off',
+    // this seems very broken atm, false positives
+    '@typescript-eslint/unbound-method': 'off'
+  }
+};
