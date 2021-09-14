1c6bbd480a939ef0566253196549cae774d0d6f4
1devNdogs <vsalgado.a@gmail.com>
2021-09-14 08:40:18 -0300

    Export subspace/types to be used on api package

---
+++ packages/types/src/index.ts
@@ -0,0 +1,31 @@
+import {
+  typesBundle as subspaceTypesBundle,
+  types as subspaceTypes,
+  typesAlias as subspaceTypeAlias,
+  rpc as subspaceRpc,
+} from '@subspace/type-definitions';
+
+import {
+  OverrideBundleType,
+  OverrideModuleType,
+  RegistryTypes,
+  DefinitionRpc,
+  DefinitionRpcSub
+} from '@polkadot/types/types';
+
+import './interfaces/augment-api';
+import './interfaces/augment-api-consts';
+import './interfaces/augment-api-query';
+import './interfaces/augment-api-rpc';
+import './interfaces/augment-api-tx';
+import './interfaces/augment-types';
+
+
+export const types: RegistryTypes = subspaceTypes;
+
+export const rpc: Record<string, Record<string, DefinitionRpc | DefinitionRpcSub>> = subspaceRpc;
+
+export const typesAlias: Record<string, OverrideModuleType> = subspaceTypeAlias;
+
+export const typesBundle = subspaceTypesBundle as unknown as OverrideBundleType;
+
