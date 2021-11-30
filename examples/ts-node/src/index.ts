b4bdb42fcb484bea6e85055756d8264638231500
1devndogs <leo@subspace.network>
2021-11-30 20:24:17 -0300

    Update ts-node example, add top-level await

--- examples/ts-node/src/index.ts
+++ examples/ts-node/src/index.ts
@@ -5,31 +5,26 @@ const NODE_WS_PROVIDER = "ws://localhost:9944";
 const FARMER_WS_PROVIDER = "ws://localhost:9955";
 const suri = "//Alice";
 
-(async () => {
-  const data = await fs.readFile("./sample-input.jpg");
-  await usage(new Uint8Array(data));
-  process.exit(1);
-})();
+const data = await fs.readFile("./sample-input.jpg");
+const objectData = new Uint8Array(data);
 
-async function usage(objectData: Uint8Array) {
-  // Generate an Identity, we create from a random account for this example.
-  const identity = await Identity.fromUri(suri);
+// Generate an Identity, we create from a random account for this example.
+const identity = await Identity.fromUri(suri);
 
-  // Generate a SubspaceClient and connect to node and farmer rpc endpoints.
-  const subspaceClient = await SubspaceClient.connect(
-    identity,
-    NODE_WS_PROVIDER,
-    FARMER_WS_PROVIDER
-  );
+// Generate a SubspaceClient and connect to node and farmer rpc endpoints.
+const subspaceClient = await SubspaceClient.connect(
+  identity,
+  NODE_WS_PROVIDER,
+  FARMER_WS_PROVIDER
+);
 
-  // Put the file as (Uint8Array) in to the objectStore and return the objectId
-  const objectId: string = await subspaceClient.putObject(objectData);
-  console.log("objectId", objectId);
+// Put the file as (Uint8Array) in to the objectStore and return the objectId
+const objectId: string = await subspaceClient.putObject(objectData);
+console.log("objectId", objectId);
 
-  // Using the objectId get the file as (Uint8Array) from the objectStore.
-  const object: Uint8Array = await subspaceClient.getObject(objectId);
+// Using the objectId get the file as (Uint8Array) from the objectStore.
+const object: Uint8Array = await subspaceClient.getObject(objectId);
 
-  // Write the file as a copy from the original
-  await fs.writeFile("./sample-from-objectStore.jpg", object, {});
-  console.log("writeFile ./sample-from-objectStore.jpg");
-}
+// Write the file as a copy from the original
+await fs.writeFile("./sample-from-objectStore.jpg", object, {});
+console.log("writeFile ./sample-from-objectStore.jpg");
