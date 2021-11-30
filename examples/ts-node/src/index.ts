096d42ccc7cdf54671308582298d9f7a3eaf99ed
1devndogs <leo@subspace.network>
2021-11-30 16:03:46 -0300

    Update ts-node, use fs promises for read and write files

--- examples/ts-node/src/index.ts
+++ examples/ts-node/src/index.ts
@@ -1,42 +1,35 @@
 import { SubspaceClient, Identity } from "subspace.js";
-import { mnemonicGenerate } from "@polkadot/util-crypto";
-import * as fs from "fs";
-import * as dotenv from "dotenv";
-dotenv.config();
+import { promises as fs } from "fs";
 
-const NODE_WS_PROVIDER = process.env.NODE_WS_PROVIDER;
-const FARMER_WS_PROVIDER = process.env.FARMER_WS_PROVIDER;
-const mnemonic = mnemonicGenerate();
+const NODE_WS_PROVIDER = "ws://localhost:9944";
+const FARMER_WS_PROVIDER = "ws://localhost:9955";
+const suri = "//Alice";
 
-fs.readFile("./sample-input.jpg", {}, async (err, data) => {
-  if (err) throw err;
+(async () => {
+  const data = await fs.readFile("./sample-input.jpg");
   await usage(new Uint8Array(data));
-});
+  process.exit(1);
+})();
 
 async function usage(objectData: Uint8Array) {
-  try {
-    // Generate an Identity, we create from a random account for this example.
-    const identity = await Identity.fromUri(mnemonic);
+  // Generate an Identity, we create from a random account for this example.
+  const identity = await Identity.fromUri(suri);
 
-    // Generate a SubspaceClient and connect to node and farmer rpc endpoints.
-    const subspaceClient = await SubspaceClient.connect(
-      identity,
-      NODE_WS_PROVIDER,
-      FARMER_WS_PROVIDER
-    );
+  // Generate a SubspaceClient and connect to node and farmer rpc endpoints.
+  const subspaceClient = await SubspaceClient.connect(
+    identity,
+    NODE_WS_PROVIDER,
+    FARMER_WS_PROVIDER
+  );
 
-    // Put the file as (Uint8Array) in to the objectStore and return the objectId
-    const objectId: string = await subspaceClient.putObject(objectData);
-    console.log("objectId", objectId);
+  // Put the file as (Uint8Array) in to the objectStore and return the objectId
+  const objectId: string = await subspaceClient.putObject(objectData);
+  console.log("objectId", objectId);
 
-    // Using the objectId get the file as (Uint8Array) from the objectStore.
-    const object: Uint8Array = await subspaceClient.getObject(objectId);
+  // Using the objectId get the file as (Uint8Array) from the objectStore.
+  const object: Uint8Array = await subspaceClient.getObject(objectId);
 
-    // Write the file as a copy from the original
-    fs.writeFile("./sample-from-objectStore.jpg", object, {}, () => {
-      console.log("writeFile ./sample-from-objectStore.jpg");
-    });
-  } catch (e) {
-    console.error(e);
-  }
+  // Write the file as a copy from the original
+  await fs.writeFile("./sample-from-objectStore.jpg", object, {});
+  console.log("writeFile ./sample-from-objectStore.jpg");
 }
