eb21538a849b945983ed4f767b6c0f1b042617dc
serge <isserge@protonmail.com>
2022-08-30 15:29:25 +0000

    update node example (#23)

--- examples/ts-node/src/index.ts
+++ examples/ts-node/src/index.ts
@@ -5,27 +5,33 @@ const NODE_WS_PROVIDER = "ws://localhost:9944";
 const FARMER_WS_PROVIDER = "ws://localhost:9955";
 const suri = "//Alice";
 
-const data = await fs.readFile("./sample-input.jpg");
-const objectData = new Uint8Array(data);
+try {
+  const data = await fs.readFile("./sample-input.jpg");
+  const objectData = new Uint8Array(data);
 
-// Generate an Identity, we create from a random account for this example.
-const identity = await Identity.fromUri(suri);
+  // Generate an Identity, we create from a random account for this example.
+  const identity = await Identity.fromUri(suri);
 
-// Generate a SubspaceClient and connect to node and farmer rpc endpoints.
-const subspaceClient = await SubspaceClient.connect(
-  identity,
-  NODE_WS_PROVIDER,
-  FARMER_WS_PROVIDER
-);
+  // Generate a SubspaceClient and connect to node and farmer rpc endpoints.
+  const subspaceClient = await SubspaceClient.connect(
+    identity,
+    NODE_WS_PROVIDER,
+    FARMER_WS_PROVIDER
+  );
 
-// Put the file as (Uint8Array) in to the objectStore and return the objectId
-const objectId: string = await subspaceClient.putObject(objectData);
-console.log("objectId", objectId);
+  // Put the file as (Uint8Array) in to the objectStore and return the objectId
+  const objectId: string = await subspaceClient.putObject(objectData);
+  console.log("objectId", objectId);
+  
+  // Using the objectId get the file as (Uint8Array) from the objectStore.
+  const object: Uint8Array = await subspaceClient.getObject(objectId);
 
-// Using the objectId get the file as (Uint8Array) from the objectStore.
-// Please, note: Archiving takes 100-120 blocks to complete, the object is not retrievable right away
-const object: Uint8Array = await subspaceClient.getObject(objectId);
-
-// Write the file as a copy from the original
-await fs.writeFile("./sample-from-objectStore.jpg", object, {});
-console.log("writeFile ./sample-from-objectStore.jpg");
+  // Write the file as a copy from the original
+  await fs.writeFile("./sample-from-objectStore.jpg", object, {});
+  console.log("writeFile ./sample-from-objectStore.jpg");
+} catch (error) {
+  if (error.message.includes('not found')) {
+    console.log('Archiving takes 100-120 blocks to complete, the object is not retrievable right away')
+  }
+  console.log(error);
+}
